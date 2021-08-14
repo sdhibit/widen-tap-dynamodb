4a2e2b888bfa47de1d6b587859b161484a62bd8a
Josh Lloyd <jlloyd@ip-10-0-0-10.ec2.internal>
2021-08-13 21:06:47 -0600

    basic code for discovering streams

---
+++ tap_dynamodb/sync_strategies/full_table.py
@@ -0,0 +1,107 @@
+"""
+Taken heavily from https://github.com/singer-io/tap-dynamodb/blob/master/tap_dynamodb/sync_strategies/full_table.py
+"""
+
+import time
+
+import singer
+from singer import metadata
+from tap_dynamodb import dynamodb
+from tap_dynamodb.deserialize import Deserializer
+
+LOGGER = singer.get_logger()
+
+
+def scan_table(table_name, projection, last_evaluated_key, config, inf_rcrds_num=None):
+    scan_params = {
+        'TableName': table_name,
+        'Limit': inf_rcrds_num if inf_rcrds_num else 1000
+    }
+
+    if projection is not None and projection != '':
+        scan_params['ProjectionExpression'] = projection
+    if last_evaluated_key is not None:
+        scan_params['ExclusiveStartKey'] = last_evaluated_key
+
+    client = dynamodb.get_client(config)
+    has_more = True
+
+    while has_more:
+        LOGGER.info(f'Scanning table {table_name} with params:')
+        for key, value in scan_params.items():
+            LOGGER.info(f'\t{key} = {value}')
+
+        result = client.scan(**scan_params)
+        yield result
+
+        if result.get('LastEvaluatedKey'):
+            scan_params['ExclusiveStartKey'] = result['LastEvaluatedKey']
+
+        has_more = result.get('LastEvaluatedKey', False)
+
+
+def sync(config, state, stream):
+    table_name = stream['tap_stream_id']
+
+    # before writing the table version to state, check if we had one to begin with
+    first_run = singer.get_bookmark(state, table_name, 'version') is None
+
+    # last run was interrupted if there is a last_id_fetched bookmark
+    was_interrupted = singer.get_bookmark(state,
+                                          table_name,
+                                          'last_evaluated_key') is not None
+
+    # pick a new table version if last run wasn't interrupted
+    if was_interrupted:
+        stream_version = singer.get_bookmark(state, table_name, 'version')
+    else:
+        stream_version = int(time.time() * 1000)
+
+    state = singer.write_bookmark(state,
+                                  table_name,
+                                  'version',
+                                  stream_version)
+    singer.write_state(state)
+
+    # For the initial replication, emit an ACTIVATE_VERSION message
+    # at the beginning so the records show up right away.
+    if first_run:
+        singer.write_version(table_name, stream_version)
+
+    last_evaluated_key = singer.get_bookmark(state,
+                                             table_name,
+                                             'last_evaluated_key')
+
+    md_map = metadata.to_map(stream['metadata'])
+    projection = metadata.get(md_map, (), 'tap-mongodb.projection')
+
+    rows_saved = 0
+
+    deserializer = Deserializer()
+    for result in scan_table(table_name, projection, last_evaluated_key, config):
+        for item in result.get('Items', []):
+            rows_saved += 1
+            # TODO: Do we actually have to put the item we retreive from
+            # dynamo into a map before we can deserialize?
+            record = deserializer.deserialize_item(item)
+            record_message = singer.RecordMessage(stream=table_name,
+                                                  record=record,
+                                                  version=stream_version)
+
+            singer.write_message(record_message)
+        if result.get('LastEvaluatedKey'):
+            state = singer.write_bookmark(state, table_name, 'last_evaluated_key', result.get('LastEvaluatedKey'))
+            singer.write_state(state)
+
+    state = singer.clear_bookmark(state, table_name, 'last_evaluated_key')
+
+    state = singer.write_bookmark(state,
+                                  table_name,
+                                  'initial_full_table_complete',
+                                  True)
+
+    singer.write_state(state)
+
+    singer.write_version(table_name, stream_version)
+
+    return rows_saved
